23573a35362172c576e5f2159d186ff66e949ab9
WAY2\david.desmaisons <david.desmaisons@way2.com.br>
2018-01-12 20:11:01 -0200

    Correct stan-alobe buid

--- Music.Cover.Finder/View/src/routeDefinitions.js
+++ Music.Cover.Finder/View/src/routeDefinitions.js
@@ -2,7 +2,6 @@ import about from './pages/about.vue'
 import search from './pages/search.vue'
 import authentication from './pages/authentication.vue'
 
-
 const routeDefinitions = [
     {name:'search', component: search, menu: {icon: 'search'}},
     {name:'authentication', component: authentication, menu: {icon: 'person'}},
