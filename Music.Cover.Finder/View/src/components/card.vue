ce7e3cacbeaf2fce544eb8c9ca0516b4a2fe4ebc
WAY2\david.desmaisons <david.desmaisons@way2.com.br>
2018-02-28 08:38:19 -0300

    Adding card component

---
+++ Music.Cover.Finder/View/src/components/card.vue
@@ -0,0 +1,71 @@
+<template>
+<section class="card-container" :style="{width: width+'px', height: height+'px', perspective: perspective+'px'}">
+  <div class="card" :class="{flipped}" :style="{transition: 'transform '+transition+'s'}">
+    <figure class="front">
+        <slot name="front">
+        </slot> 
+    </figure>
+    <figure class="back">
+        <slot name="back">
+        </slot>
+    </figure>
+  </div>
+</section>
+</template>
+<script>
+const props = {
+  flipped: {
+    required: false,
+    default: false
+  },
+  width: {
+    required: false,
+    type: Number,
+    default: 150
+  },
+  height: {
+    required: false,
+    type: Number,
+    default: 150
+  },
+  perspective: {
+    required: false,
+    type: Number,
+    default: 800
+  },
+  transition: {
+    required: false,
+    type: Number,
+    default: 1
+  }
+};
+export default {
+  props
+};
+</script>
+<style scoped>
+.card-container {
+  position: relative;
+}
+.card {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  transform-style: preserve-3d;
+}
+.card figure {
+  margin: 0;
+  display: block;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+}
+.card .back {
+  transform: rotateY(180deg);
+}
+.card.flipped {
+  transform: rotateY(180deg);
+}
+</style>
+
--- Music.Cover.Finder/View/src/components/disc.vue
+++ Music.Cover.Finder/View/src/components/disc.vue
@@ -1,22 +1,19 @@
 <template>
-<section class="disc-container">
-  <div class="card">
-    <figure class="front">
-      <div class="disc-container" :style="{background}">
-      <div class="data">
-          <div class="content">
-            <h1 class="title black--text">{{artist}}</h1>
-            <h1 class="title red--text">{{discTitle}}</h1>
-          </div>
-        </div>
-      </div>
-	  </figure>
-    <figure class="back">
-    </figure>
-  </div>
-</section>
+<card :height="150" :width="150" class="disc-item" @mouseover.native="flipped=true" @mouseleave.native="flipped=false" :flipped="flipped">
+	<div slot="front" class="front" :style="{background}">
+	</div>
+	
+	<div slot="back" class="data">
+		<div class="content">
+			<h1 class="title black--text">{{artist}}</h1>
+			<h1 class="title red--text">{{discTitle}}</h1>
+		</div>
+	</div>
+</card>
 </template>
 <script>
+import card from "./card";
+
 const props = {
   disc: {
     required: true,
@@ -24,7 +21,15 @@ const props = {
   }
 };
 export default {
+  components: {
+    card
+  },
   props,
+  data() {
+    return {
+      flipped: false
+    };
+  },
   methods: {
     splitTitle() {
       return this.disc.title.split(" - ");
@@ -45,15 +50,20 @@ export default {
   }
 };
 </script>
-<style <style scoped>
-img {
-  max-height: 150px;
-  max-width: 150px;
-}
-.disc-container {
+<style scoped>
+.disc-item {
   margin: 10px;
-  width: 150px;
-  height: 150px;
+}
+.data {
+  margin: 0;
+  background: grey;
+  width: 100%;
+  height: 100%;
+}
+.front {
+  margin: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
 
