fe042466b0863954d959e599071cc54e8653e51a
desmaisons_david <desmaisons_david@hotmail.com>
2018-03-03 09:41:52 -0300

    Improving UI layout

--- Music.Cover.Finder/View/src/pages/search.vue
+++ Music.Cover.Finder/View/src/pages/search.vue
@@ -1,51 +1,51 @@
 <template>
-    <main>
-      <v-content>
-        <v-container fluid class="main-container">
-          <v-flex lg10 offset-lg1>
+  <main>
+    <v-content>
+      <v-container fluid class="main-container">
+        <v-flex lg10 offset-lg1>
 
-            <v-card>
-              <v-layout row wrap>
-                <v-flex lg6>
-                    <v-text-field box :label="$t('Resource.Album')" v-model="viewModel.Album" @keyup.enter.native="submit" required :autofocus="autoFocus"></v-text-field>
-                  </v-flex>
-                  <v-flex lg6>
-                    <v-text-field box :label="$t('Resource.Artist')" v-model="viewModel.Artist" @keyup.enter.native="submit" required></v-text-field>
-                  </v-flex>
-              </v-layout>
-              <v-layout row wrap>
-                  <v-flex>
-                    <text-button :text="$t('Resource.Search')" :command="viewModel.Search" :loading="viewModel.Running" color="blue"></text-button>
-                  </v-flex>
-              </v-layout>
-             </v-card>
-          
-            <v-card wrap v-if="viewModel.Results.length>0">
-              <v-toolbar color="blue">
-                <v-toolbar-title>{{resultsFound}}</v-toolbar-title>
-              </v-toolbar>
-                <div v-resize:debounce.25="onResize">
-                  <isotope ref="isotope" :options='getIsotopeOptions()' :list="viewModel.Results">
-                    <disc v-for="(item,index) in viewModel.Results" :key="index" :disc="item">
-                    </disc>
-                  </isotope>
-                </div>
-            </v-card>
-
-            <v-card wrap v-if="viewModel.NothingFound">
-              <v-toolbar color="red">
-                <v-toolbar-title>{{$t('Resource.NoResultFound')}}</v-toolbar-title>
-              </v-toolbar>
-	          </v-card>
-
+          <v-card>
             <v-layout row wrap>
+              <v-flex lg6>
+                <v-text-field box :label="$t('Resource.Album')" v-model="viewModel.Album" @keyup.enter.native="submit" required :autofocus="autoFocus"></v-text-field>
+              </v-flex>
+              <v-flex lg6>
+                <v-text-field box :label="$t('Resource.Artist')" v-model="viewModel.Artist" @keyup.enter.native="submit" required></v-text-field>
+              </v-flex>
+            </v-layout>
+            <v-layout row wrap>
+              <v-flex>
+                <text-button :text="$t('Resource.Search')" :command="viewModel.Search" :loading="viewModel.Running" color="blue"></text-button>
+              </v-flex>
+            </v-layout>
+          </v-card>
+
+          <v-card wrap v-if="viewModel.Results.length>0">
+            <v-toolbar color="blue">
+              <v-toolbar-title>{{resultsFound}}</v-toolbar-title>
+            </v-toolbar>
+            <div class="isotope-container" v-resize:debounce.25="onResize">
+              <isotope class="isotope-root" ref="isotope" :options='getIsotopeOptions()' :list="viewModel.Results">
+                <disc v-for="(item,index) in viewModel.Results" :key="index" :disc="item">
+                </disc>
+              </isotope>
+            </div>
+          </v-card>
+
+          <v-card wrap v-if="viewModel.NothingFound">
+            <v-toolbar color="red">
+              <v-toolbar-title>{{$t('Resource.NoResultFound')}}</v-toolbar-title>
+            </v-toolbar>
+          </v-card>
+
+          <v-layout row wrap>
           </v-layout>
 
-          </v-flex>
+        </v-flex>
 
-        </v-container>
-      </v-content>
-    </main>
+      </v-container>
+    </v-content>
+  </main>
 </template>
 
 <script>
@@ -109,6 +109,14 @@ main {
 .main-container {
   height: 100%;
   overflow-y: auto;
+}
+.isotope-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.isotope-root {
+  width: 100%;
 }
 </style>
 
